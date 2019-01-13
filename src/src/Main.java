836f29fd479d7964b63de75b2833a65f8256a969
lahiver <46652190+lahiver@users.noreply.github.com>
2019-01-13 21:50:08 +0300

    Update Main.java

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,7 +1,8 @@
 // какое то задание
 // следишь? жмешь коммит
+// задание получил
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello Alina");
     }
 }
