adc164e2bbb6023324ff1eaa67d44aab2c532aa0
Alina <lahiver@mail.ru>
2019-01-14 10:11:13 +0300

    //реализовать многопоточное приложение, реализующее вывод всех четных слов из списка файлов (порядка 20 файлов). Для каждого файла создается новый поток, но общее число потоков не должно превышать 10.

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,8 +1,3 @@
-// какое то задание
-// следишь? жмешь коммит
-// задание получил
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello Alina");
+
     }
-}
